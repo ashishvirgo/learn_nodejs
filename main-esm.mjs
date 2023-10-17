efc6b4367793c825d75f8f3b14a886e9b35e57f6
ashish.bajpai <ashish.bajpai@ksolves.com>
2023-10-17 17:43:10 +0530

    ESM Demo

---
+++ main-esm.mjs
@@ -0,0 +1,14 @@
+// import math from "./math-esm.mjs";
+
+// // console.log("sum=",math.add(25,25));
+// // console.log("subtract=",math.sub(25,25));
+// // console.log("multiply=",math.mul(25,4));
+// const {add,sub,mul}=math;
+// console.log("sum=",add(2,2));
+// console.log("subtract=",sub(2,2));
+// console.log("multiply=",mul(2,4));
+
+import {add,sub,mul} from "./math-esm.mjs";
+console.log("sum=",add(1,1));
+console.log("subtract=",sub(2,1));
+console.log("multiply=",mul(3,4));
---
+++ math-esm.mjs
@@ -0,0 +1,22 @@
+// const add=(a,b)=>{
+//     return a+b;
+// };
+// const sub=(a,b)=>{
+//     return a-b;
+// };
+// const mul=(a,b)=>{
+//     return a*b;
+// };
+// export default {
+//     add,sub,mul
+// }
+
+export const add=(a,b)=>{
+    return a+b;
+};
+export const sub=(a,b)=>{
+    return a-b;
+};
+export const mul=(a,b)=>{
+    return a*b;
+};
