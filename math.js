ab9d0de3b7dbe301c821d6b17694b87f5fdb1d1d
ashish.bajpai <ashish.bajpai@ksolves.com>
2023-10-17 17:15:26 +0530

    diff patterns import export modules

--- math.js
+++ math.js
@@ -1,14 +1,33 @@
-const add=(a,b)=>{
+exports.add=(a,b)=>{
     return a+b;
-};
-const sub=(a,b)=>{
-    return a-b;
-};
-const mul=(a,b)=>{
-    return a*b;
-};
-module.exports={
-    add,
-    sub,
-    mul
-};
+  };
+exports.sub=(a,b)=>{
+      return a-b;
+    };
+exports.mul=(a,b)=>{
+      return a*b;
+    };
+// module.exports.add=(a,b)=>{
+//       return a+b;
+//     };
+//     module.exports.sub=(a,b)=>{
+//         return a-b;
+//       };
+//       module.exports.mul=(a,b)=>{
+//         return a*b;
+//       };
+
+// const add=(a,b)=>{
+//     return a+b;
+// };
+// const sub=(a,b)=>{
+//     return a-b;
+// };
+// const mul=(a,b)=>{
+//     return a*b;
+// };
+// module.exports={
+//     add,
+//     sub,
+//     mul
+// };
--- mathdemo.js
+++ mathdemo.js
@@ -1,4 +1,9 @@
 const math=require('./math');
-console.log("sum=",math.add(25,23));
-console.log("subtract=",math.sub(25,23));
-console.log("multiply=",math.mul(25,2));
+const {add,sub,mul}=math;
+// console.log("sum=",math.add(25,23));
+// console.log("subtract=",math.sub(25,23));
+// console.log("multiply=",math.mul(25,2));
+
+console.log("sum=",add(25,23));
+console.log("subtract=",sub(25,23));
+console.log("multiply=",mul(25,2));
